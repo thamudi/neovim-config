7009bf862236021c276910d58c5fefeee16db7d8
thamudi <tamim_hamoudi@outlook.com>
2025-04-18 06:34:30 +0300

    chore: add a bunch of plugins for nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -19,4 +19,55 @@ return {
   { "nvim-telescope/telescope.nvim", tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
+   -- Markdown Preview
+  { "iamcco/markdown-preview.nvim",
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
+  -- Treesitter
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "nvim-treesitter/nvim-treesitter-context", lazy = false, config = function()
+      require("treesitter-context").setup{}
+  end },
+  -- Git
+  { "lewis6991/gitsigns.nvim", config = function()
+      require("gitsigns").setup{}
+  end },
+  -- Autopairs
+  { "windwp/nvim-autopairs", config = function()
+      require("nvim-autopairs").setup{}
+  end },
+  -- Comment
+  { "numToStr/Comment.nvim", config = function()
+      require("Comment").setup{}
+  end },
+  -- File Explorer
+  { "kyazdani42/nvim-tree.lua", config = function()
+      require("nvim-tree").setup{}
+  end },
+  -- Status Line
+  { "nvim-lualine/lualine.nvim", config = function()
+      require("lualine").setup{}
+  end },
+  -- Buffer Line
+  { "akinsho/bufferline.nvim", config = function()
+      require("bufferline").setup{}
+  end },
+  -- Which Key
+  { "folke/which-key.nvim", config = function()
+      require("which-key").setup{}
+  end },
+  -- Terminal
+  { "akinsho/toggleterm.nvim", config = function()
+      require("toggleterm").setup{}
+  end },
+  -- Harpoon
+  { "ThePrimeagen/harpoon", config = function()
+      require("harpoon").setup{}
+  end },
+  -- GitHub Copilot
+  { "github/copilot.vim", config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
+  end },
 }
