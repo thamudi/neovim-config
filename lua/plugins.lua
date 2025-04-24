4b7ee9a37b166323d56fee4e95a6d3a98ac43a0a
thamudi <tamim_hamoudi@outlook.com>
2025-04-24 21:44:30 +0300

    chore: add prettier config to null-ls to format yaml files

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -23,7 +23,12 @@ return {
     {
         "jose-elias-alvarez/null-ls.nvim",
         config = function()
-            require("null-ls").setup {}
+            local null_ls = require("null-ls")
+            null_ls.setup {
+                sources = {
+                    null_ls.builtins.formatting.prettier.with({ filetypes = { "yaml" } }),
+                }
+            }
         end
 
     },
